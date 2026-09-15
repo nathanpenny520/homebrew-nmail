5dcc3fbc56e0814bbf556bc2b337882be7f87752
Nathan Penny <nathanpenny@qq.com>
2026-09-15 17:03:26 +0800

    nmail 0.4.1

--- Formula/nmail.rb
+++ Formula/nmail.rb
@@ -1,10 +1,10 @@
 class Nmail < Formula
   desc "AI-driven, local-first aggregated email client"
   homepage "https://github.com/nathanpenny520/Nmail"
-  url "https://github.com/nathanpenny520/Nmail/releases/download/v0.4.0/nmail-macos-arm64"
-  sha256 "70d4e2239170d52059b1c5f10526806608337781cad27385fcc5e77fa052a3d2"
+  url "https://github.com/nathanpenny520/Nmail/releases/download/v0.4.1/nmail-macos-arm64"
+  sha256 "48338c8499dbd72e4703c93853f09d469d61b2af9d937a8439e7d9f73d989a77"
   license "MIT"
-  version "0.4.0"
+  version "0.4.1"
 
   depends_on :macos
 
